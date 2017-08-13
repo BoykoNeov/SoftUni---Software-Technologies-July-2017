3c3caedb50e7c2f909f97e395b2e561754ff9652
BoykoNeov <boikoneov@gmail.com>
2017-08-13 21:51:23 +0300

    :pineapple: adding completed taks "numbers 1 to 50" and "TODO list"

---
+++ C#/TODOList/TODOList/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using TODOList.Models;
+
+namespace TODOList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (TaskDbContext db = new TaskDbContext())
+            {
+                List<Task> allTasks = db.Tasks.ToList();
+                return View(allTasks);
+            }
+        }
+    }
+}
---
+++ C#/TODOList/TODOList/Controllers/TaskController.cs
@@ -0,0 +1,43 @@
+﻿using System.Web.Mvc;
+using TODOList.Models;
+
+namespace TODOList.Controllers
+{
+    public class TaskController : Controller
+    {
+        [HttpPost]
+        public ActionResult Create(Task task)
+        {
+            if (task != null)
+            {
+                using (var db = new TaskDbContext())
+                {
+                    db.Tasks.Add(task);
+                    db.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Delete (int? id)
+        {
+            if (id != null)
+            {
+                using (var db = new TaskDbContext())
+                {
+                    Task taskToDelete = db.Tasks.Find(id);
+
+                    if (taskToDelete != null)
+                    {
+                        db.Tasks.Remove(taskToDelete);
+                        db.SaveChanges();
+                    }
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
