06e78ef261109f4eb700e0fb5ff55deeefbc9d7f
BoykoNeov <boikoneov@gmail.com>
2017-08-15 22:22:30 +0300

    :cat2: Added article details view

--- C#/Blog/Blog/Controllers/ArticleController.cs
+++ C#/Blog/Blog/Controllers/ArticleController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 
 namespace Blog.Controllers
@@ -22,6 +23,29 @@ namespace Blog.Controllers
                 List<Article> articles = db.Articles.Include(a => a.Author).ToList();
 
                 return View(articles);
+            }
+        }
+
+        //Get: Article/Details/
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (BlogDbContext db = new BlogDbContext())
+            {
+                Article currentArticle = db.Articles.Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .FirstOrDefault();
+
+                if (currentArticle == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(currentArticle);
             }
         }
     }
