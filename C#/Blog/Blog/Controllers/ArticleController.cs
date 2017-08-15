6ab8b9c51675a1e2d054f9a9d51827a81a42be1b
BoykoNeov <boikoneov@gmail.com>
2017-08-15 22:08:28 +0300

    :four_leaf_clover: to the point of successfully listing articles

---
+++ C#/Blog/Blog/Controllers/ArticleController.cs
@@ -0,0 +1,30 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        // Get: Article/List
+        public ActionResult List()
+        {
+            using (BlogDbContext db = new BlogDbContext())
+            {
+                List<Article> articles = db.Articles.Include(a => a.Author).ToList();
+
+                return View(articles);
+            }
+        }
+    }
+}
---
+++ C#/Blog/Blog/Models/Article.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Models
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        [ForeignKey("Author")]
+        public string AutorId { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public Article()
+        {
+            this.DateAdded = DateTime.Now;
+        }
+    }
+}
